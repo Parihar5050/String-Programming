c806d5c84cbfa3ceed8dde2fc5b3216f56ebb4fe
Aditya Parihar <adityapariharparihar@gmail.com>
2023-01-16 20:11:39 +0530

    same or !

---
+++ CheckString.java
@@ -0,0 +1,16 @@
+public class CheckString 
+{
+public static void main(String[] args) 
+{
+    
+}
+static boolean areAlmostEqual(String s1, String s2) 
+{
+ if (s1.equals(s2) || s2.equals(s1)) return true;
+ 
+ 
+ 
+
+ return false;
+}    
+}
